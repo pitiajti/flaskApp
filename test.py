1f2a5baeb81380fd4ff64a8e119303da6e13b673
pitiajti <pitiajti@gmail.com>
2023-07-25 21:59:36 +0200

    updating test python file

--- test.py
+++ test.py
@@ -1,7 +1,30 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "Index page"
+    
+@app.route("/hello")
+def hello():
+	return """ <!DOCTYPE html>
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html> 
+"""
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+	# show the user profile for that user
+	return f'User {escape(username)}'
